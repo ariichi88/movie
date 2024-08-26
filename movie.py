52d4ac37a4bfd236c16ff6c4f191b248776ab891
ariichi88 <ariichi88@gmail.com>
2024-08-26 20:10:34 +0900

    条件分岐の変更

--- movie.py
+++ movie.py
@@ -26,9 +26,7 @@ def CopyMp4Files(date, newname):
     count = 1
     for _, fromfile in enumerate(fromfiles):
         _, ext = os.path.splitext(fromfile)
-        if 'jpg' in ext:
-            pass
-        else:
+        if 'mp4' in ext:
             tofile = newname + '-' + format(count, '02') + ext
             shutil.copy2(FromDir + fromfile, ToDir + '/' + tofile)
             count = count + 1
